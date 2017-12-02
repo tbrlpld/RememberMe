fc36541dc512fc9cf53b9477be7df040830cae15
Tibor Leupold <tibor.leupold@gmail.com>
2017-12-02 01:30:19 +0100

    feat: Create a simple test file with a square card and a centered image
    Created the example to understand the different parameters for the positioning.
    Example has to be transfered to actual files for memory game.
    Example is based on: http://www.mademyday.de/css-height-equals-width-with-pure-css.html

--- js/app.js
+++ js/app.js
@@ -7,10 +7,13 @@ function runAfterDOMIsBuild(){
 	for (var row = 1; row <= cardsPerRow; row++) {
 		for (var column = 1; column <= cardsPerColumn; column++) {
 			// $(".card-area").append('<div class="memory-card"><img src="https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png" width=100%></div>');
-			$(".card-area").append('<div class="memory-card">content</div>');
+			// $(".card-area").append('<div class="memory-card"><div class="card-content"><img src="https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png" class="card-image"></div></div>');
+			$(".card-area").append('<div class="memory-card"><div class="card-content">content</div></div>');
 			lastCardCreated = $('.memory-card').last();
 			lastCardCreated.css('grid-row', String(row));
 			lastCardCreated.css('grid-column', String(column));
+			lastCardCreated.css('background-image', 'url("https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png")');
+			lastCardCreated.css('background-size', 'cover');
 		}
 	}
 }
