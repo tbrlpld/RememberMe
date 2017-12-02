7d828faf3edde3b98410e2f50dd6fac34f0e5126
Tibor Leupold <tibor.leupold@gmail.com>
2017-12-02 01:42:52 +0100

    feat: Responsive layout of square cards
    Applied css and html from the simple test to the actual files.
    Simple test has been remove.
    Simple file was based on: http://www.mademyday.de/css-height-equals-width-with-pure-css.html

--- js/app.js
+++ js/app.js
@@ -8,12 +8,10 @@ function runAfterDOMIsBuild(){
 		for (var column = 1; column <= cardsPerColumn; column++) {
 			// $(".card-area").append('<div class="memory-card"><img src="https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png" width=100%></div>');
 			// $(".card-area").append('<div class="memory-card"><div class="card-content"><img src="https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png" class="card-image"></div></div>');
-			$(".card-area").append('<div class="memory-card"><div class="card-content">content</div></div>');
+			$(".card-area").append('<div class="card-spacer"><div class="card-content"><img src="https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png" class="card-image"></div></div>');
 			lastCardCreated = $('.memory-card').last();
 			lastCardCreated.css('grid-row', String(row));
 			lastCardCreated.css('grid-column', String(column));
-			lastCardCreated.css('background-image', 'url("https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png")');
-			lastCardCreated.css('background-size', 'cover');
 		}
 	}
 }
