ba51368a4feebb08b4ad49485e166e74c68f1f67
Tibor Leupold <tibor.leupold@gmail.com>
2017-12-02 23:53:43 +0100

    feat: added functionality to create random shuffled deck
    An array of 8 symbols is defined.
    This array is doubled toget 16 cards and then shuffled.
    Recourses about shuffle algorithms is added to RESOURCES.md

--- RESOURCES.md
+++ RESOURCES.md
@@ -6,3 +6,6 @@ This is a unsorted list of resources (outside of Udacity) I used to create the m
 * Pseudo element to create square cards: (http://www.mademyday.de/css-height-equals-width-with-pure-css.html)
 * Font size based on container size: (https://codepen.io/CrocoDillon/pen/fBJxu)
 * List of HTML symbols: (https://www.w3schools.com/charsets/ref_utf_symbols.asp)
+* Random integer function: (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+* Random sorting of array: (https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
+* Dustenfeld algorithm for shuffling: (https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm)
--- js/app.js
+++ js/app.js
@@ -1,14 +1,103 @@
 var cardsPerRow = 4;
 var cardsPerColumn = 4;
 
-function runAfterDOMIsBuild(){
-	var lastCardCreated;
+//-------------------------------------------------------------------------------------------------
+//
+// Random Symbol Array
+//
+//-------------------------------------------------------------------------------------------------
 
+function getArrayOfSymbols(){
+	var symbols = ['&#9728;',
+				   '&#9729;',
+				   '&#9730;',
+				   '&#9733;',
+				   '&#9752;',
+				   '&#9774;',
+				   '&#9786;',
+				   '&#9822;'
+				   ];
+	return symbols
+}
+
+function doubleArrayOfSymbols(symbols){
+	var symbolsDoubled = [];
+	symbols.forEach(function(item){
+		symbolsDoubled.push(item);
+		symbolsDoubled.push(item);
+		// console.log(symbolsDoubled)
+	});
+	return symbolsDoubled
+}
+
+// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+// Based on Dustenfeld algorithm: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+function shuffleArray(array){
+	var currentIndex = 0;
+	var randomIndex = 0;
+	var maxIndex = array.length - 1;
+	// console.log('maxIndex = ' + maxIndex);
+	var tempValue;
+
+	while (currentIndex <= maxIndex){
+		// console.log('currentIndex = ' + currentIndex);
+		randomIndex = getRandomIntInclusive(currentIndex, maxIndex);
+		// console.log('randomIndex = ' + randomIndex);
+		// console.log('Before change:');
+		// console.log('array[currentIndex] = ' + array[currentIndex]);
+		// console.log('array[randomIndex] = ' + array[randomIndex]);
+		tempValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = tempValue;
+		// console.log('After change:');
+		// console.log('array[currentIndex] = ' + array[currentIndex]);
+		// console.log('array[randomIndex] = ' + array[randomIndex]);
+		// console.log('array = ' + array);
+		currentIndex += 1;
+	}
+	return array
+}
+
+function getRandomSymbolArray(){
+	var symbols = getArrayOfSymbols();
+	// console.log(symbols);
+	var symbolsDoubled = doubleArrayOfSymbols(symbols);
+	// console.log(symbolsDoubled);
+	var symbolsShuffled = shuffleArray(symbolsDoubled);
+	// console.log(symbolsShuffled);
+	return symbolsShuffled
+}
+
+//-------------------------------------------------------------------------------------------------
+//
+// MAIN
+//
+//-------------------------------------------------------------------------------------------------
+
+function runAfterDOMIsBuild(){
+
+	var symbols = getRandomSymbolArray();
+	// console.log(symbols)
+
+	var lastCardCreated;
+	var currentCardNumber = 0;
 	for (var row = 1; row <= cardsPerRow; row++) {
+		// console.log('row = ' + row)
 		for (var column = 1; column <= cardsPerColumn; column++) {
-			// $(".card-area").append('<div class="memory-card"><img src="https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png" width=100%></div>');
-			// $(".card-area").append('<div class="memory-card"><div class="card-content"><img src="https://icon-icons.com/icons2/614/PNG/512/wifi-symbol-inside-a-circle_icon-icons.com_56445.png" class="card-image"></div></div>');
-			$(".card-area").append('<div class="card-spacer"><div class="card-content"><div class="card-symbol">&#9824</div></div></div>');
+			// console.log('column = ' + column)
+			currentCardNumber += 1;
+			// console.log('currentCardNumber = ' + currentCardNumber)
+			symbolIndex = currentCardNumber - 1;
+			// console.log('symbolIndex = ' + symbolIndex)
+			currentSymbol = symbols[symbolIndex];
+			console.log('Card ' + currentCardNumber + ' --> ' + currentSymbol);
+			$(".card-area").append('<div class="card-spacer"><div class="card-content"><div class="card-symbol">' + currentSymbol + '</div></div></div>');
 			lastCardCreated = $('.memory-card').last();
 			lastCardCreated.css('grid-row', String(row));
 			lastCardCreated.css('grid-column', String(column));
